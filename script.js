acedf49d8faa44a8962d360cd1ecdb6fe9d93564
rachelstephi <87799664+rachelstephi@users.noreply.github.com>
2026-07-06 00:02:40 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,36 @@
 //your JS code here. If required.
+const form = document.getElementById("form");
+const ageInput = document.getElementById("age");
+const nameInput = document.getElementById("name");
+
+form.addEventListener("submit", function (e) {
+    e.preventDefault();
+
+    const age = ageInput.value.trim();
+    const name = nameInput.value.trim();
+
+    // Validation
+    if (age === "" || name === "") {
+        alert("Please enter valid details.");
+        return;
+    }
+
+    // Promise
+    const votingPromise = new Promise((resolve, reject) => {
+        setTimeout(() => {
+            if (Number(age) > 18) {
+                resolve(`Welcome, ${name}. You can vote.`);
+            } else {
+                reject(`Oh sorry ${name}. You aren't old enough.`);
+            }
+        }, 4000);
+    });
+
+    votingPromise
+        .then((message) => {
+            alert(message);
+        })
+        .catch((message) => {
+            alert(message);
+        });
+});
